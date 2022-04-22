a81999b0b74a9c88e42479f9e69a0a8006c10115
Sam Johnson <sam@howldesign.com>
2022-04-21 22:13:00 -0500

    Adding a loading concept for when the word list is first read.  Also reset functionality

--- app/src/main/java/com/howldesign/cheatle_android/CheatleViewModel.kt
+++ app/src/main/java/com/howldesign/cheatle_android/CheatleViewModel.kt
@@ -1,12 +1,18 @@
 package com.howldesign.cheatle_android
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.toMutableStateList
+import android.content.Context
+import android.renderscript.ScriptGroup
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.InputStream
 
 enum class BoxColors { white, gray, yellow, green }
 
@@ -22,6 +28,39 @@ class CheatleViewModel: ViewModel() {
     private val _boxes = getAllBoxes().toMutableStateList()
     val boxes: List<List<CheatleBoxData>>
         get() = _boxes
+
+    private var _allWords = mutableListOf<String>()
+
+    private val _listOfWords = listOf<String>().toMutableStateList()
+    val listOfWords: List<String>
+        get() = _listOfWords
+
+    private val _isLoading = mutableStateOf(true)
+    val isLoading = _isLoading
+
+    fun loadWords(context: Context) {
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val inputStream: InputStream = context.assets.open("english5letterwords.txt")
+                val lineList = mutableListOf<String>()
+                inputStream.bufferedReader().forEachLine { lineList.add(it) }
+
+                launch(Dispatchers.Main) {
+                    lineList.forEach{
+                        _listOfWords.add(it)
+                        _allWords.add(it)
+                    }
+                    _isLoading.value = false
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+                launch(Dispatchers.Main) {
+                    _isLoading.value = false
+                }
+            }
+        }
+    }
+
 
     fun changeColor(box: CheatleBoxData, wordIndex: Int){
         val updatedWord: MutableList<CheatleBoxData> = _boxes.get(wordIndex).toMutableList()
@@ -45,6 +84,22 @@ class CheatleViewModel: ViewModel() {
         }
     }
 
+    fun filter(){
+//TODO: figure out the filtering.  Probably the same as in iOS
+
+    }
+
+    fun reset(){
+        val defaultBoxes = getAllBoxes()
+        for (wordIndex in 0..4){
+            _boxes.set(
+                wordIndex,
+                defaultBoxes[wordIndex]
+            )
+        }
+
+        this.filter()
+    }
 }
 private fun getAllBoxes() = List(5){ i -> List(5){ j -> CheatleBoxData(j) } }
 
--- app/src/main/java/com/howldesign/cheatle_android/MainActivity.kt
+++ app/src/main/java/com/howldesign/cheatle_android/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
@@ -32,17 +35,31 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     CheatleApp(cheatleViewModel = vm)
+
                 }
             }
         }
     }
 }
 
+@Composable
+fun LoadingScreen(){
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+
+        Spacer(modifier = Modifier.height(10.dp))
+        CircularProgressIndicator()
+        Text("Loading ...")
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
+
+
 
 @Composable
 fun CheatleApp(
     cheatleViewModel: CheatleViewModel = viewModel()
 ) {
+    val context = LocalContext.current
 
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -58,6 +75,37 @@ fun CheatleApp(
             )
         }
 
+        when {
+            cheatleViewModel.isLoading.value -> LoadingScreen()
+            else -> {
+                Row {
+                    Button(
+                        onClick = { cheatleViewModel.filter() }
+                    ){
+                        Text("Filter")
+                    }
+                    Spacer(Modifier.width(10.dp))
+                    Button(
+                        onClick = { cheatleViewModel.reset() }
+                    ){
+                        Text("Reset")
+                    }
+                }
+
+                LazyColumn{
+                    items(cheatleViewModel.listOfWords){ wordItem ->
+                        Text(wordItem)
+                    }
+
+                }
+            }
+        }
+
+
+    }
+
+    LaunchedEffect(true){
+        cheatleViewModel.loadWords(context)
     }
 }
 
